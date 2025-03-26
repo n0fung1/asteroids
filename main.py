bb68fd9ec8968f56a80084a02275ad9e02860175
n0fung1 <n0fung1@proton.me>
2025-03-26 19:50:55 -0400

    feat: Movementgit add .!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,12 +9,29 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    px = SCREEN_WIDTH / 2
+    py = SCREEN_HEIGHT / 2
+
+    player = Player(px, py)
+    player
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+
+        player.update(dt)
         screen.fill(000000)
+        player.draw(screen)
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
+
 
     
 
